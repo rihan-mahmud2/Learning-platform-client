86fcf4751cffb097a475b5176682e93dd3b9bb79
rihan-mahmud2 <rihanmahmud1415@gmail.com>
2022-10-26 14:17:30 +0600

    added course info

--- src/Others/CourseDetails.js
+++ src/Others/CourseDetails.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import { Button } from "@material-tailwind/react";
 
 const CourseDetails = () => {
   const details = useLoaderData();
-  const { id, img, subject, lecturer, lectures } = details;
+  const { id, img, subject, lecturer, lectures, description } = details;
   return (
-    <div>
-      <h1>This is the details page {lecturer}</h1>
+    <div className="mt-12 p-10">
+      <h1 className="text-2xl text-gray-700 font-extrabold">
+        Become a wise in {subject}
+      </h1>
+      <img
+        src={img}
+        alt="thumbnail"
+        className="w-1/4 border mx-auto my-10 border-black rounded-full"
+      ></img>
+      <p className="text-left mx-2">{description}</p>
+      <div className="flex justify-center items-center mt-10">
+        <span className="mr-5">Mentor: {lecturer}</span>
+        <span>The Number of Lectures {lectures}</span>
+      </div>
+      <Button className="mt-10">Get Premium Access</Button>;
     </div>
   );
 };
